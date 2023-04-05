8369ac76ee9553f7cefa3c76621b8643c7bd5aac
3Pad <github@3pad.xyz>
2023-04-05 15:47:43 -0400

    Git Push FIx

--- git_push.sh
+++ git_push.sh
@@ -4,8 +4,8 @@ git init
 git add --all
 current_date=$(date)
 git commit -m "Automatic Commit & Update at $current_date"
-git config user.email "github@3pad.xyz"
-git config user.name "3Pad"
+git config user.email "$GITHUB_EMAIL"
+git config user.name "$GITHUB_USER"
 git remote add origin https://3Pad:"$GITHUB_KEY"@github.com/3Pad/sites.git
 git remote set-url origin https://3Pad:"$GITHUB_KEY"@github.com/3Pad/sites.git
 git fetch origin
