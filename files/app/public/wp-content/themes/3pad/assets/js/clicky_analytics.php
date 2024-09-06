2c3a74a535406eb602bde5725603450c39f53fad
3Pad <github@3pad.xyz>
2024-09-06 01:02:00 -0400

    Playground Update

--- files/app/public/wp-content/themes/3pad/assets/js/clicky_analytics.php
+++
@@ -1,2 +0,0 @@
-<!-- Clicky tag (.js) -->
-<script async src=" //static.getclicky.com/<?php $settings_page = get_queried_object_id(); echo get_field('clicky_analytics', $settings_page); ?>.js"></script>
--- files/app/public/wp-content/themes/3pad/assets/js/disqus-reactions.php
+++
@@ -1,6 +0,0 @@
-<?php $did = get_queried_object_id(); ?>
-
-<div id="disqus_thread" class="disqus_reactions"></div>
-<script defer nonce="<?php echo $jsnonce = wp_create_nonce('js-nonce'); ?>">
-	var disqus_config = function () { this.page.url = "<?php echo site_url(); ?>", this.page.identifier = "<?php echo $did; ?>" }; !function () { var e = document, t = e.createElement("script"); t.src = 'https://<?php echo get_field("disqus_shortname"); ?>.disqus.com/embed.js', t.setAttribute("data-timestamp", +new Date), (e.head || e.body).appendChild(t) }();
-</script>
